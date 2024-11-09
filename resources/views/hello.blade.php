0fe96d6c17a8dbfafa16513b8c1604c7b4413076
Mursalin Hasnain Razmi <mursalin@gtsb.com.my>
2024-11-09 21:37:52 +0600

    clean commit

--- resources/views/hello.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('master')
-
-@section('title', 'Hello')
-
-@section('content')
-    <h1>Hello, world!</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return view('hello');
-});
 
 Route::resource('/product', ProductController::class);
